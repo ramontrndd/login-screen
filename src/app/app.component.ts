e0db6880468f812462169e493c219b28ff468d18
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-07-08 09:06:42 -0300

    feat: add libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'login-screen';
+
+  constructor(private ngxService: NgxUiLoaderService) {}
+
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,45 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgxUiLoaderConfig, NgxUiLoaderModule, PB_DIRECTION, SPINNER } from 'ngx-ui-loader';
 
+import { MaterialModule } from '../shared/material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  "bgsColor": "#4d6eff",
+  "bgsOpacity": 0.5,
+  "bgsPosition": "top-right",
+  "bgsSize": 60,
+  "bgsType": "three-strings",
+  "blur": 3,
+  "delay": 0,
+  "fastFadeOut": true,
+  "fgsColor": "#4d6eff",
+  "fgsPosition": "center-center",
+  "fgsSize": 100,
+  "fgsType": "ball-spin-fade-rotating",
+  "gap": 24,
+  "logoPosition": "center-center",
+  "logoSize": 80,
+  "logoUrl": "",
+  "masterLoaderId": "master",
+  "overlayBorderRadius": "0",
+  "overlayColor": "rgba(40, 40, 40, 0.8)",
+  "pbColor": "#4d6eff",
+  "pbDirection": "ltr",
+  "pbThickness": 3,
+  "hasProgressBar": true,
+  "text": "Analisando seu dados...",
+  "textColor": "#4d6eff",
+  "textPosition": "center-center",
+  "maxTime": -1,
+  "minTime": 300
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +47,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
